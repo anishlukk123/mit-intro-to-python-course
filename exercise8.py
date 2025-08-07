df41d581e1bda7a2f3fe0cf004697ff7565acffd
Anish Lukkireddy <anishlukkireddy@Anishs-MacBook-Pro.local>
2025-08-07 12:10:13 -0400

    exercise8.py

---
+++ exercise8.py
@@ -0,0 +1,27 @@
+def same_chars(s1, s2):
+    """
+    s1 and s2 are strings
+    Returns boolean True is a character in s1 is also in s2, and vice 
+    versa. If a character only exists in one of s1 or s2, returns False.
+    """
+
+
+    character = set()
+    character2  = set()
+
+   
+
+    for char in s1:
+         character.add(char)
+
+    for char in s2:
+        character2.add(char)
+
+    return character == character2
+    # Your code here
+
+# Examples:
+print(same_chars("abc", "cab"))     # prints True
+print(same_chars("abccc", "caaab")) # prints True
+print(same_chars("abcd", "cabaa"))  # prints False
+print(same_chars("abcabc", "cabz")) # prints False
